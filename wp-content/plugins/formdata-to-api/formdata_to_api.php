50e2ad5859c66e455c8410295b2e7e4aedb32dbf
Israel David Dahan <id@studiodvd.co.il>
2024-01-10 21:34:52 +0200

    change to send data

--- wp-content/plugins/formdata-to-api/formdata_to_api.php
+++ wp-content/plugins/formdata-to-api/formdata_to_api.php
@@ -27,67 +27,51 @@ Author URI: http://israeldahan.co.il/
 // 	dbDelta( $sql );
 // }
 
-add_filter( 'ninja_forms_submit_data', 'send_data_to_api' );
 
-function send_data_to_api( $form_data ) {
- 
-  $full_name = '';
-  $email = '';
-  $phone = '';
-  $message = '';
-  $form_id = $form_data[ 'id' ]; // Form ID.
-  if ( $form_id == 2 ) { // Form ID.
-    foreach( $form_data[ 'fields' ] as $field ) { // Field settigns, including the field key and value.
-      $field_id = $field[ 'id' ]; // Field ID.
-      $field_key = $field[ 'key' ]; // Field key.
-      $field_value = $field[ 'value' ]; // Field value.
-      if ($field_key == 'fullname'){
-        $full_name = $field_value;
-      }
-      if ($field_key == 'email'){
-        $email = $field_value;
-      }
-      if ($field_key == 'phone'){
-        $phone = $field_value;
-      }
-      if ($field_key == 'message'){
-        $message = $field_value;
-      }
-    }
-  };
-  // foreach( $form_data[ 'fields' ] as $field ) { // Field settigns, including the field key and value.
-  //   $field_id = $field[ 'id' ]; // Field ID.
-  //   $field_key = $field[ 'key' ]; // Field key.
-  //   $field_value = $field[ 'value' ]; // Field value.
-  // }
+add_action( 'ninja_forms_after_submission', 'my_ninja_forms_after_submission' );
 
-  $url = 'https://app.seker.live/fm1/form-data';
-  $data = array(
-    'externalId' => 'KEfdt', 
-    'fullName' => $full_name, 
-    'email' => $email, 
-    'phone' => $phone, 
-    'details' => $message, 
-    'sourceName' => 'landingPageWordpress');
+function my_ninja_forms_after_submission( $form_data ){
+  // Do stuff.
+  $form_title = $form_data['settings'][ 'title' ];
 
-    http://localhost:9020/?page_id=40    // use key 'http' even if you send the request to https://...
-  $options = [
-      'http' => [
-          'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-          'method' => 'POST',
-          'content' => http_build_query($data),
-      ],
-  ];
+  if ( $form_title == 'send_to_feedback' ) {
+      echo("send_to_feedback");
+      $fields_by_key = $form_data["fields_by_key"];
 
-  $context = stream_context_create($options);
-  $result = file_get_contents($url, false, $context);
-  
-  if ($result === false) {
-      /* Handle error */
+      $full_name = $fields_by_key["fullname"]["value"];
+      $email = $fields_by_key["email"]["value"];
+      $phone = $fields_by_key["phone"]["value"];
+      $subject = $fields_by_key["subject"]["value"];
+      $message = $fields_by_key["message"]["value"];
+      $fields_by_key["req_id"]["value"] = wp_generate_uuid4();
+
+      $url = 'https://app.seker.live/fm1/form-data';
+      $data = array(
+        'externalId' => 'KEfdt', 
+        'fullName' => $full_name, 
+        'email' => $email, 
+        'phone' => $phone, 
+        'subject' => $subject,
+        'details' => $message, 
+        'sourceName' => 'landingPage' + ' ' + $fields_by_key["req_id"]["value"]
+      );
+
+      // use key 'http' even if you send the request to https://...
+      $options = [
+          'http' => [
+              'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+              'method' => 'POST',
+              'content' => http_build_query($data),
+          ],
+      ];
+
+      $context = stream_context_create($options);
+      $result = file_get_contents($url, false, $context);
+      
+      if ($result === false) {
+          /* Handle error */
+          
+      }
   }
-  // $form_settings = $form_data[ 'settings' ]; // Form settings.
-  
-  // $extra_data = $form_data[ 'extra' ]; // Extra data included with the submission.
-  
-  return $form_data;
 }
+
